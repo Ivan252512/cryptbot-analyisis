0043924e1c90d64fda37b32132641e8be74d51cb
Ivan2512 <ivan31416neda@gmail.com>
2023-03-18 02:36:14 -0600

    5 min graphics

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-function openSection(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName(
-    "profile__section__tab__tabcontent"
-  );
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+$(".images img").click(function(){
+  $("#full-image").attr("src", $(this).attr("src"));
+  $('#image-viewer').show();
+});
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+$("#image-viewer .close").click(function(){
+  $('#image-viewer').hide();
+});
